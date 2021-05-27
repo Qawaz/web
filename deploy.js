0b644820fcea4bcf852ecbfbd0bec7fa356e4bf0
Waqas Tahir <wakaztahir@gmail.com>
2021-05-27 22:16:31 +0500

    new deploy

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 const ghpages = require('gh-pages')
 
 ghpages.publish('public', {
-  branch: 'main',
+  branch: 'public',
   repo: 'https://github.com/timeline-notes/timeline-notes.github.io.git'
 }, () => {
   console.log('Deployed "public" to  "main" branch in Github')
